f58b4d16e3a9ef6a60a8607f1be8b7ca48814cca
bongshim <bongshime@gmail.com>
2024-12-30 23:23:13 +0100

    feat: enhance SubmitLeadForm component with improved styling and layout
    - Centered the heading text for better visual alignment.
    - Adjusted the width of the input field and button for a more responsive design.
    - Updated button text size for consistency across different screen sizes.
    - Enhanced the overall layout to improve user experience and accessibility.

--- src/components/landing/SubmitLeadForm.tsx
+++ src/components/landing/SubmitLeadForm.tsx
@@ -28,7 +28,7 @@ const SubmitLeadForm = ({
             height={250}
             className='mb-2'
           />
-          <h1 className='text-2xl md:text-3xl lg:text-4xl font-extrabold mb-3'>
+          <h1 className='text-2xl md:text-3xl lg:text-4xl font-extrabold mb-3 text-center'>
             We are Coming Soon!
           </h1>
           <p className='text-center text-sm text-darkPurple md:w-10/12 mx-auto font-sans'>
@@ -36,11 +36,11 @@ const SubmitLeadForm = ({
             the first to experience it
           </p>
 
-          <div className='flex relative gap-2 border border-[#ECECEC] rounded-full mx-auto w-[85%] h-[60px] p-1 my-10 focus-within:border-lemon'>
+          <div className='flex relative gap-2 border border-[#ECECEC] rounded-full mx-auto w-[95%] md:w-[85%] h-[60px] p-1 my-10 focus-within:border-lemon'>
             <input
               type='email'
               placeholder='Enter your email'
-              className='w-[80%] p-2 rounded-full border border-transparent outline-none bg-transparent'
+              className='w-[95%] md:w-[80%] p-2 rounded-full border border-transparent outline-none bg-transparent text-sm md:text-base'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -70,7 +70,7 @@ const SubmitLeadForm = ({
                   variant: 'success',
                 });
               }}
-              className='bg-lemon text-darkPurple px-8 whitespace-nowrap rounded-full hover:transform hover:scale-[1.02] hover:shadow-sm transition-transform duration-300 ease-in-out'
+              className='bg-lemon text-sm md:text-base text-darkPurple px-4 md:px-8 whitespace-nowrap rounded-full hover:transform hover:scale-[1.02] hover:shadow-sm transition-transform duration-300 ease-in-out'
             >
               {isLoading ? 'Submitting...' : 'Join Waitlist'}
             </button>
